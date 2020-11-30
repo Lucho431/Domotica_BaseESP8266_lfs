ea67e788d9cc5479783710c8113a617e5b7aefbb
Lucho_431 <luciano.salvatore431@gmail.com>
2020-11-29 21:04:33 -0300

    update: prueba inicial de la unidad central 20201129

--- central_broker_ESP8266/central_broker_ESP8266.ino
+++ central_broker_ESP8266/central_broker_ESP8266.ino
@@ -1,49 +1,195 @@
 /*
- * uMQTTBroker demo for Arduino
+ * uMQTTBroker demo for Arduino (C++-style)
  * 
- * Minimal Demo: the program simply starts a broker and waits for any client to connect.
+ * The program defines a custom broker class with callbacks, 
+ * starts it, subscribes locally to anything, and publishs a topic every second.
+ * Try to connect from a remote client and publish something - the console will show this as well.
  */
 
 #include <ESP8266WiFi.h>
 #include "uMQTTBroker.h"
 
-uMQTTBroker myBroker;
+#define TICK_PERIOD     10 //en ms.
+
+
+typedef enum{
+    TRYING_WIFI,
+    WIFI_TIMING_OUT,
+    TRYING_MQTT,
+    MQTT_TIMING_OUT,
+    ALL_CONNECTED,
+}T_CONN;
+
+typedef enum{
+    AUTO,
+    MANUAL,
+}T_MODE;
+
+typedef enum{
+    LOW_L,
+    HIGH_L,
+    FALL,
+    RISE,
+}T_INPUT;
+    
 
 /*
- * Your WiFi config here
+ * Custom broker class with overwritten callback functions
  */
+class myMQTTBroker: public uMQTTBroker
+{
+public:
+    virtual bool onConnect(IPAddress addr, uint16_t client_count) {
+      Serial.println(addr.toString()+" connected");
+      return true;
+    }
+    
+    virtual bool onAuth(String username, String password) {
+      Serial.println("Username/Password: "+username+"/"+password);
+      return true;
+    }
+    
+    virtual void onData(String topic, const char *data, uint32_t length) {
+      char data_str[length+1];
+      os_memcpy(data_str, data, length);
+      data_str[length] = '\0';
+      
+      Serial.println("received topic '"+topic+"' with data '"+(String)data_str+"'");
+    }
+};
+
+
+
+//variables red
 char ssid[] = "ESP8266_CU";      // your network SSID (name)
 char pass[] = "RJQ-729!!"; // your network password
-
-/*
- * WiFi init stuff
- */
+bool WiFiAP = true;      // Do yo want the ESP as AP? (useless)
 
 
-void startWiFiAP()
-{
-  WiFi.softAP(ssid, pass);
-  Serial.println("AP started");
-  Serial.println("IP address: " + WiFi.softAPIP().toString());
+myMQTTBroker myBroker;
+
+
+//variables timer
+uint8_t flag_tick = 0;
+unsigned long last_tick = 0;
+
+//variables conexion
+T_CONN conn_status = TRYING_WIFI;
+uint16_t reconnect_time = 0; // 1 * 10ms
+
+
+
+void connections_handler() {
+    
+    switch(conn_status){
+        
+        case TRYING_WIFI:
+            // We start by connecting to a WiFi network
+            Serial.println();
+            Serial.print("Starting access point.");
+            Serial.println(ssid);
+
+            WiFi.mode(WIFI_AP);
+            if (!WiFi.mode(WIFI_AP)){
+                Serial.print(".");
+                conn_status = WIFI_TIMING_OUT;
+                reconnect_time = 500;
+            }else{
+                Serial.println("");
+                Serial.println("Access point ready");
+                Serial.println("IP address: " + WiFi.softAPIP().toString());
+                reconnect_time = 0;
+                conn_status = TRYING_MQTT;
+            }
+        break;
+        
+        case WIFI_TIMING_OUT:
+            
+            if (!WiFi.mode(WIFI_AP)){            
+                if (!reconnect_time){
+                    conn_status = TRYING_WIFI;
+                }
+            }else{
+                Serial.println("");
+                Serial.println("Access point ready");
+                Serial.println("IP address: " + WiFi.softAPIP().toString());
+                reconnect_time = 0;
+                conn_status = TRYING_MQTT;
+            }
+                
+        break;
+        
+        case TRYING_MQTT:
+            
+            if (!WiFi.mode(WIFI_AP)){
+                Serial.println("Wifi AP lost. restarting...");
+                conn_status = TRYING_WIFI;
+                break;
+            }
+            
+			// Starting the broker
+			Serial.println("Starting MQTT broker");
+			myBroker.init();
+            
+            //Subscribe to all topics
+			myBroker.subscribe("#");
+			
+			conn_status = ALL_CONNECTED;
+
+        break;
+        
+        case ALL_CONNECTED:
+            if (!WiFi.mode(WIFI_AP)){
+                Serial.println("Wifi AP lost. restarting...");
+                conn_status = TRYING_WIFI;
+                break;
+            }
+            
+        break;
+        
+        default:
+        break;        
+        
+    }//end switch
+}//end connections_handler
+
+void timer_update(void){
+    
+    unsigned long now = millis();
+    
+    if(now > last_tick + TICK_PERIOD){
+        flag_tick = 1;
+        last_tick = now;        
+    }
 }
+  
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println();
+	Serial.begin(115200);
+	Serial.println();
+	Serial.println();
 
-  //start the ESP as AP
-  startWiFiAP();
-
-  // Start the broker
-  Serial.println("Starting MQTT broker");
-  myBroker.init();
+	connections_handler();
 }
 
 void loop()
-{   
-  // do anything here
-  delay(1000);
-}
+{
+	timer_update();
+	
+	if (flag_tick){
+				
+		if (reconnect_time) reconnect_time--;
+		
+		
+		
+		flag_tick = 0;
+	}//end if flag_tick
+	
+    connections_handler();
+	
+	//Publish the counter value as String
+	//myBroker.publish("broker/counter", (String)counter++);
+
+}//end loop
 
