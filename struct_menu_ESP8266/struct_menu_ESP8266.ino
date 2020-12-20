72b3a5d5279db6093c89198e5dd1cbdbf4dccb77
Lucho_431 <luciano.salvatore431@gmail.com>
2020-12-20 18:27:32 -0300

    estructura de menú completa

--- struct_menu_ESP8266/struct_menu_ESP8266.ino
+++ struct_menu_ESP8266/struct_menu_ESP8266.ino
@@ -7,6 +7,12 @@
 #define D7 7
 
 
+void cmd_modo (void);
+void cmd_ldr (void);
+void cmd_contraste (void);
+void cmd_salvaPantalla (void);
+void cmd_tecla (void);
+
 
 
 typedef struct t_menu_general {
@@ -27,7 +33,6 @@ typedef enum{
 } T_ESTATUS_PANTALLA;
 	
 
-
 T_MENU_GENERAL listaMenus[20]= {
 									{"      MENU      ", NULL, &listaMenus[1], 2, 0, NULL},  //0
 									
@@ -41,7 +46,7 @@ T_MENU_GENERAL listaMenus[20]= {
 									{"   CONTRASTE    ", &listaMenus[2], NULL, 0, 0, cmd_contraste},  //6
 									{"SALVA PANTALLAS ", &listaMenus[2], NULL, 0, 0, cmd_salvaPantalla},  //7
 };
-	
+
 
 T_ESTATUS_PANTALLA status_pantalla = NAVEGANDO;	
 int8_t opcion = 0;
@@ -52,28 +57,28 @@ LiquidCrystal lcd(RS,EN,D4,D5,D6,D7);
 
 String renglon1, renglon2;
 
+//variables de la luz de afuera:
+uint8_t mode = 0;
+uint8_t mode_selected = 0;
+uint8_t teclaLuz = 0;
+uint8_t teclaLuz_selected = 0;
+int LDR_actual = 512;
 
 
 void imprime_pantalla (void){
 	
-	//Serial.print("\n" + menuActual->nombre + "\n");
 	lcd.clear();
 	lcd.setCursor(0,0);
-	lcd.print(menuActual->nombre);
-		
+	lcd.print(renglon1);
 	lcd.setCursor(0,1);
-	if (menuActual->firstSubMenu != NULL){
-		T_MENU_GENERAL* aux = menuActual->firstSubMenu + menuActual->seleccion;
-		//Serial.print(aux->nombre + "\n");
-		lcd.print(aux->nombre);
-	} else{
-		//Serial.print("naranja...      ");
-		lcd.print("naranja...      ");
-	}
+	lcd.print(renglon2);
+	
 }//fin imprime_pantalla
 
 
 void navega_menu (void){
+	
+	T_MENU_GENERAL* aux;
 	
 	char cmd = (char) Serial.read();
 		
@@ -84,11 +89,12 @@ void navega_menu (void){
 			if (menuActual->menuPadre != NULL){ 
 				
 				menuActual = menuActual->menuPadre;
+				opcion = menuActual->seleccion;
+				
 				
 				renglon1 = menuActual->nombre;
-				T_MENU_GENERAL* aux = menuActual->firstSubMenu + menuActual->seleccion;
+				aux = menuActual->firstSubMenu + menuActual->seleccion;
 				renglon2 = aux->nombre;
-				
 				flag_imprimir = 1;
 			}
 		
@@ -96,14 +102,17 @@ void navega_menu (void){
 		
 		case '2':
 			//Serial.print("abajo\n");
+			
+			menuActual = menuActual->firstSubMenu + menuActual->seleccion;
+			opcion = menuActual->seleccion;
+			
 			if (menuActual->firstSubMenu != NULL){ 
-				
-				menuActual = menuActual->firstSubMenu + menuActual->seleccion;
-				opcion = 0;
-				
+							
 				renglon1 = menuActual->nombre;
-				
+				aux = menuActual->firstSubMenu + menuActual->seleccion;
+				renglon2 = aux->nombre;
 				flag_imprimir = 1;
+				
 			}else if (menuActual->accion != NULL){
 				status_pantalla = SETEANDO_CMD;
 				menuActual->accion();
@@ -118,6 +127,9 @@ void navega_menu (void){
 			if (opcion < 0) opcion = menuActual->totalSubMenus - 1;
 			menuActual->seleccion = opcion;
 			
+			renglon1 = menuActual->nombre;
+			aux = menuActual->firstSubMenu + menuActual->seleccion;
+			renglon2 = aux->nombre;
 			flag_imprimir = 1;			
 		break;
 		
@@ -127,6 +139,9 @@ void navega_menu (void){
 			if (opcion >= menuActual->totalSubMenus) opcion = 0;
 			menuActual->seleccion = opcion;
 			
+			renglon1 = menuActual->nombre;
+			aux = menuActual->firstSubMenu + menuActual->seleccion;
+			renglon2 = aux->nombre;
 			flag_imprimir = 1;			
 		break;
 		
@@ -137,58 +152,251 @@ void navega_menu (void){
 
 void cmd_modo (void){
 
+	//static uint8_t mode = 0;
+	//static uint8_t mode_selected = 0;
+
 	char cmd = (char) Serial.read();
 		
+	renglon1 = "      MODO      ";
+	
 	switch(cmd){
-		
-		case '8':
-			//Serial.print("arriba\n");	
-			if (!menuNivel) menuNivel--;			
-		
-		break;
-		
-		case '2':
-			//Serial.print("abajo\n");
-			if (menuNivel < MENU_NIEVL_MAX - 1) menuNivel++;
-		
+		default:
+			if (mode){				
+				if (mode_selected == mode){
+					renglon2 = "   AUTOMATICO X ";
+				}else{
+					renglon2 = "   AUTOMATICO   ";
+				}				
+			}else{
+				if (mode_selected == mode){
+					renglon2 = "     MANUAL  X  ";
+				}else{
+					renglon2 = "     MANUAL     ";
+				}
+			}
+			
+			flag_imprimir = 1;
 		break;
 		
 		case '4':
-			//Serial.print("izquierda\n");
-			
-		
-		break;
-		
 		case '6':
-			//Serial.print("derecha\n");
-		
+			mode = ~mode;
+			
+			if (mode){				
+				if (mode_selected == mode){
+					renglon2 = "   AUTOMATICO X ";
+				}else{
+					renglon2 = "   AUTOMATICO   ";
+				}				
+			}else{
+				if (mode_selected == mode){
+					renglon2 = "     MANUAL  X  ";
+				}else{
+					renglon2 = "     MANUAL     ";
+				}
+			}
+			
+			flag_imprimir = 1;
 		break;
 		
-	}//end switch
-	
+		case '2':
+			if (mode){
+				renglon2 = "   AUTOMATICO X ";
+				//mando el MQTTcorrespondiente
+			}else{
+				renglon2 = "     MANUAL  X  ";
+				//mando el otro MQTT correspondiente.
+			}
+			flag_imprimir = 1;
+			
+			mode_selected = mode;
+		break;
+		
+		case '8':
+			//Serial.print("arriba\n");	
+			status_pantalla = NAVEGANDO;			
+			
+			renglon2 = menuActual->nombre;
+			menuActual = menuActual->menuPadre;	
+			renglon1 = menuActual->nombre;
+			opcion = menuActual->seleccion;
+			flag_imprimir = 1;
+		
+		break;
+	}
 	
 }//fin cmd_modo
 
 
 void cmd_ldr (void){
+
+	renglon1 = "luz actual: " + String(LDR_actual, DEC);
 	
+	char cmd = (char) Serial.read();
+		
+	switch(cmd){
+		default:
+			renglon2 = "Valor: nada";
+		break;
+		
+		case '4':
+			flag_imprimir = 1;
+			renglon2 = "izquierda";
+		break;
+		
+		case '2':
+			flag_imprimir = 1;
+			renglon2 = "abajo";
+		break;
+		
+		case '6':
+			flag_imprimir = 1;
+			renglon2 = "derecha";
+		break;
+		
+		case '8':
+			//Serial.print("arriba\n");	
+			status_pantalla = NAVEGANDO;			
+			
+			renglon2 = menuActual->nombre;
+			menuActual = menuActual->menuPadre;	
+			renglon1 = menuActual->nombre;
+			opcion = menuActual->seleccion;
+			flag_imprimir = 1;
+		
+		break;
+	}
 	
 }//fin cmd_ldr
 
 
 void cmd_tecla (void){
 	
+	renglon1 = "TECLA DE LA LUZ ";
+	
+	char cmd = (char) Serial.read();
+		
+	switch(cmd){
+		default:
+			if (teclaLuz){				
+				renglon2 = "       ON       ";
+			}else{
+				renglon2 = "       OFF      ";
+			}				
+						
+			flag_imprimir = 1;
+		break;
+		
+		case '4':
+		case '6':
+		case '2':
+			teclaLuz = ~teclaLuz;
+			
+			if (teclaLuz){				
+				renglon2 = "       ON       ";
+				//manda el MQTT
+			}else{
+				renglon2 = "       OFF      ";
+				//manda el MQTT
+			}				
+						
+			flag_imprimir = 1;
+		break;
+		
+		case '8':
+			//Serial.print("arriba\n");	
+			status_pantalla = NAVEGANDO;			
+			
+			renglon2 = menuActual->nombre;
+			menuActual = menuActual->menuPadre;	
+			renglon1 = menuActual->nombre;
+			opcion = menuActual->seleccion;
+			flag_imprimir = 1;
+		
+		break;
+	}
 	
 }//fin cmd_tecla
 
 void cmd_contraste (void){
 	
+	renglon1 = "no se puede regular!";
 	
+	char cmd = (char) Serial.read();
+		
+	switch(cmd){
+		default:
+			renglon2 = "Valor: " + cmd;
+		break;
+		
+		case '4':
+			flag_imprimir = 1;
+			renglon2 = "izquierda";
+		break;
+		
+		case '2':
+			flag_imprimir = 1;
+			renglon2 = "abajo";
+		break;
+		
+		case '6':
+			flag_imprimir = 1;
+			renglon2 = "derecha";
+		break;
+		
+		case '8':
+			//Serial.print("arriba\n");	
+			status_pantalla = NAVEGANDO;			
+			
+			renglon2 = menuActual->nombre;
+			menuActual = menuActual->menuPadre;	
+			renglon1 = menuActual->nombre;
+			opcion = menuActual->seleccion;
+			flag_imprimir = 1;
+		
+		break;
+	}
 }//fin cmd_contraste
 
 
 void cmd_salvaPantalla (void){
+
+	renglon1 = "acá no hay nada...";
 	
+	char cmd = (char) Serial.read();
+		
+	switch(cmd){
+		default:
+			renglon2 = "Valor: " + cmd;
+		break;
+		
+		case '4':
+			flag_imprimir = 1;
+			renglon2 = "izquierda";
+		break;
+		
+		case '2':
+			flag_imprimir = 1;
+			renglon2 = "abajo";
+		break;
+		
+		case '6':
+			flag_imprimir = 1;
+			renglon2 = "derecha";
+		break;
+		
+		case '8':
+			//Serial.print("arriba\n");	
+			status_pantalla = NAVEGANDO;			
+			
+			renglon2 = menuActual->nombre;
+			menuActual = menuActual->menuPadre;	
+			renglon1 = menuActual->nombre;
+			opcion = menuActual->seleccion;
+			flag_imprimir = 1;
+		
+		break;
+	}	
 	
 }//fin cmd_salvaPantalla
 
@@ -200,6 +408,9 @@ void setup() {
 	lcd.begin(16, 2);
 	
 	menuActual = &listaMenus[0];
+	
+	renglon1 = "      MENU      ";
+	renglon2 = "   LUZ AFUERA   ";
 	
 	imprime_pantalla();
 	
